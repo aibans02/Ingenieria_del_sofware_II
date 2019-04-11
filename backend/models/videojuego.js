4c4e17cd9004e7ab26b1f842eeb151a60f516430
Armando <aibans02@estudiantes.unileon.es>
2019-04-11 17:29:52 +0200

    Plantilla para construir los modelos

--- backend/models/videojuego.js
+++ backend/models/videojuego.js
@@ -12,12 +12,21 @@ module.exports = (sequelize, DataType) => {
         notEmpty: true,
       },
     },
+    ID_USUARIO: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'USUARIO', // 'persons' refers to table name
+        key: 'ID_USUARIO', // 'id' refers to column name in persons table
+     }
+    },
     done: {
       type: DataType.BOOLEAN,
       allowNull: false,
       defaultValue: false,
     },
   }, {
+    tableName: 'Videojuego',
     classMethods: {
       associate: (models) => {
         Videojuego.belongsTo(models.Users);
