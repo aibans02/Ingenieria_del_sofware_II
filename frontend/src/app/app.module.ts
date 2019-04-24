9957a145b45acb856205526ba349a3153c302e10
idomim01 <idomim01@estudiantes.unileon.es>
2019-04-24 16:22:56 +0200

    Añadidos varios componentes (solo inicializados)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { RegisterComponent } from './register/register.component';
+import { GameComponent } from './game/game.component';
+import { NewsUpdatesComponent } from './news-updates/news-updates.component';
+import { ForumsComponent } from './forums/forums.component';
+import { GuidesComponent } from './guides/guides.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    RegisterComponent,
+    GameComponent,
+    NewsUpdatesComponent,
+    ForumsComponent,
+    GuidesComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
