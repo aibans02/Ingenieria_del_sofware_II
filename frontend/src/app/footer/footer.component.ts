cb3a5ad14920626c7ecfd834df6f529e345a0983
Armando <aibans02@estudiantes.unileon.es>
2019-05-10 19:00:52 +0200

    Cambios en el footer

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'twitter',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/twitter.svg'));
+    iconRegistry.addSvgIcon(
+      'facebook',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/facebook.svg'));
+    iconRegistry.addSvgIcon(
+      'instagram',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/instagram.svg'));
+    iconRegistry.addSvgIcon(
+      'googleplus',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/img/icons/googleplus.svg'));
+  }
 
   ngOnInit() {
   }
