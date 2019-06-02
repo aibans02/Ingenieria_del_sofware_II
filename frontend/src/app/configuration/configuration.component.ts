e180e2436ee2dc810246e47979e09b9d95d6b302
idomim01 <idomim01@estudiantes.unileon.es>
2019-06-02 21:30:58 +0200

    configuracion vista v+, boton contestar foros

--- frontend/src/app/configuration/configuration.component.ts
+++ frontend/src/app/configuration/configuration.component.ts
@@ -8,6 +8,8 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class ConfigurationComponent implements OnInit {
 
+  token = localStorage.getItem('token');
+
   nick = new FormControl('', [Validators.required])
   email = new FormControl('', [Validators.required, Validators.email]);
   password = new FormControl('', [Validators.required])
